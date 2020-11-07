64ea58da38e3e1bd4325a0ec2e9d94afb28185fe
Luca Tassinari <luca.tassinari.2000@gmail.com>
2020-11-07 10:29:01 +0100

    Add NotEnoughBatteryException

---
+++ src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
@@ -0,0 +1,39 @@
+package it.unibo.oop.lab.exception1;
+
+public class NotEnoughBatteryException extends IllegalStateException{
+
+	private static final long serialVersionUID = 1L;
+	
+	private final double batteryLevel;
+	private final double batteryRequire;
+	
+	/**
+	 * 
+	 * @param batteryLevel
+	 * 				current battery level
+	 * @param batteryRequired
+	 * 				level required for moving
+	 */
+	public NotEnoughBatteryException(final double batteryLevel, 
+			final double batteryRequired) {
+		super();
+		this.batteryLevel = batteryLevel;
+		this.batteryRequire = batteryRequired;
+	}
+
+	/**
+	 * @return String representation of this exception 
+	 */
+	public String toString() {
+		return "Not enough battery for moving. Battery level is " + batteryLevel + 
+				", battery required is " + batteryRequire + "]";
+	}
+	
+	/**
+	 * @return String representation of this exception 
+	 */
+	public String getMessage() {
+		return this.toString();
+	}
+	
+}
