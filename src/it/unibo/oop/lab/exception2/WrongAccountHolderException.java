7a0b9dd2de11888f13063a2277ea3f129629b7cb
Luca Tassinari <luca.tassinari.2000@gmail.com>
2020-11-08 10:14:25 +0100

    Add WrongAccountHolderException

---
+++ src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -0,0 +1,21 @@
+package it.unibo.oop.lab.exception2;
+
+public class WrongAccountHolderException extends IllegalStateException {
+
+	private static final long serialVersionUID = 7190620326792084901L;
+	private final int currentId;
+	
+	public WrongAccountHolderException(final int expectedId, final int currentId) {
+		this.currentId = currentId;
+	}
+
+	public String toString() {
+		return "Operation failed: your id (" + this.currentId + ") is not "
+				+ "authorized!";
+	}
+	
+	public String getMessage() {
+		return this.toString();
+	}
+
+}
